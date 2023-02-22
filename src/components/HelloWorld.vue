0cba22d6533b525853201e8bceedd5587a060d59
dladoul amal <73843270+amal96-dl@users.noreply.github.com>
2023-02-22 15:45:34 +0100

    ToDoList commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,221 @@
+<template>
+  <div class="todo px-6">
+    <h1 class="display-2 font-weight-bold mb-3">
+    </h1>
+    <v-row class="text-center"> 
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3 blue--text">
+          Todo list
+        </h1>
+      </v-col>
+    </v-row>
+    <v-row class="text-center">
+      <v-col class="mb-4">
+        <v-divider class="mt-4">
+        </v-divider>
+        <v-row class="my-1" align="center">
+          <strong class="mx-4 text-info-darken-2">
+            Remaining: {{ remainingTasks }}
+          </strong>
+          <v-divider vertical>
+          </v-divider>
+          <strong class="mx-4 text-success-darken-2">
+            Completed: {{ completedTasks }}
+          </strong>
+          <v-spacer></v-spacer>
+          <v-progress-circular v-model="progress" class="me-2" color="green">
+          </v-progress-circular>
+        </v-row>
+        <v-divider class="mb-4"></v-divider>
+      </v-col>
+    </v-row>
+    <v-text-field outlined label="Add task" clearable hide-details v-model="newTaskTitle" @click:append="addTask" @keyup.enter="addTask" append-icon="mdi-plus-circle" class="mb-4"></v-text-field>
+      <v-list flat class="pt-0">
+        <transition-group type="transition" name="flip-list">
+          <div class="sortable" v-for="(task, i) in tasks" :key="task.id" @click="doneTask(task.id)">
+            <v-list-item :class="{ 'blue lighten-5': task.done }">
+              <template v-slot:default>
+                <v-list-item-action>
+                  <v-checkbox v-if="handle != true" :input-value="task.done"></v-checkbox>
+                  <v-icon color="primary" class="handle" v-else>mdi-drag</v-icon>
+                </v-list-item-action>
+                <v-list-item-content>
+                  <v-list-item-title :class="{ 'text-decoration-line-through': task.done }">
+                    {{ task.title }}
+                  </v-list-item-title>
+                </v-list-item-content>
+                <v-list-item-content v-if="task.dueDate" class="text-right text-uppercase">
+                  <v-list-item-title class="caption">
+                    <v-icon v-if="!task.expired || task.done" dense class="mr-1">mdi-calendar-outline</v-icon>
+                    <v-icon v-else-if="!task.done && task.expired" dense class="mr-1 error--text">mdi-calendar-alert</v-icon>
+                    <span :class="{ 'error--text font-weight-bold': task.expired && !task.done }"> {{ computedDue(task.dueDate) }}</span>
+                  </v-list-item-title>
+                </v-list-item-content>
+                <v-menu>
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-btn dark icon v-bind="attrs" v-on="on">
+                      <v-icon color="primary lighten-1">mdi-dots-vertical</v-icon>
+                    </v-btn>
+                  </template>
+                  <v-list>
+                    <v-list-item>
+                      <v-list-item-title>
+                        <div role="button" @click="(task.modal = true), (dialog = 0)">
+                          <v-btn icon>
+                            <v-icon color="grey">mdi-pencil</v-icon>
+                          </v-btn>
+                            Edit
+                        </div>
+                      </v-list-item-title>
+                    </v-list-item>
+                    <v-list-item>
+                      <v-list-item-title>
+                        <div role="button" @click="(task.modal = true), (dialog = 1)">
+                          <v-btn icon>
+                            <v-icon color="grey">mdi-calendar-clock</v-icon>
+                          </v-btn>
+                            Due Date
+                        </div>
+                      </v-list-item-title>
+                    </v-list-item>
+                    <v-list-item v-for="(item, i) in sideMenu" :key="i">
+                      <v-list-item-title role="button" v-if="(item.title = 'Delete')" @click.stop="handleFnCall(item.function, task.id)">
+                        <v-btn icon>
+                          <v-icon color="grey">mdi-{{ item.button }}</v-icon>
+                        </v-btn>
+                        {{ item.title }}
+                      </v-list-item-title>
+                    </v-list-item>
+                  </v-list>
+                </v-menu>
+              </template>
+            </v-list-item>
+            <v-divider>
+            </v-divider>
+            <v-dialog ref="dialog" v-if="dialog === 0" v-model="task.modal" :return-value.sync="task.title" persistent width="290px">
+              <v-card>
+                <v-card-title>Edit task</v-card-title>
+                <v-text-field v-model="task.title" @keyup.enter="saveTask($refs, i, task.title, 'Task updated!')" class="pa-5"></v-text-field>
+                <v-btn text color="primary" @click.stop="task.modal = false">Cancel</v-btn>
+                <v-btn text color="primary" @click.stop="saveTask($refs, i, task.title, 'Task updated!')">Save</v-btn>
+              </v-card>
+            </v-dialog>
+            <v-dialog ref="dialog" v-else v-model="task.modal" :return-value.sync="task.dueDate" persistent width="290px">
+              <v-date-picker v-model="task.dueDate" scrollable :min="today()">
+                <v-btn text color="primary" @click.stop="task.modal = false">Cancel</v-btn>
+                <v-btn text color="primary" @click.stop="saveTask($refs, i, task.dueDate, 'Due date is setted!')">OK</v-btn>
+              </v-date-picker>
+            </v-dialog>
+          </div>
+        </transition-group>
+      </v-list>
+      <div v-if="tasks.length === 0" class="my-auto text-center green--text">
+        <v-icon x-large class="green--text">mdi-check-all</v-icon>
+        <h1>No tasks</h1>
+      </div>
+      <v-snackbar v-model="snackbar.active">
+        {{ snackbar.text }}
+        <template v-slot:action="{ attrs }">
+          <v-btn color="pink" text v-bind="attrs" @click="snackbar.active = false">
+            Close 
+          </v-btn>
+        </template>
+      </v-snackbar>
+  </div>
+</template>
+<script>
+export default {
+   name: 'HelloWorld',
+  data() {
+    return {
+      handle: false,
+      newTaskTitle: "",
+      tasks: [],
+      snackbar: {
+        active: false,
+        text: String,
+      },
+      dialog: Number,
+      sideMenu: [{ title: "Delete", button: "delete", function: "deleteTask" }],
+    };
+  },
+  computed: {
+     completedTasks () {
+        return this.tasks.filter(task => task.done).length
+      },
+      progress () {
+        return this.completedTasks / this.tasks.length * 100
+      },
+      remainingTasks () {
+        return this.tasks.length - this.completedTasks
+      },
+    },
+  mounted() {
+    if (localStorage.tasks) {
+      this.tasks = JSON.parse(localStorage.tasks);
+      const today = new Date(`${this.today()} GMT-0300`);
+      this.tasks.forEach((e) => {
+        const taskDueDate = new Date(`${e.dueDate} GMT-0300`);
+        return today > taskDueDate ? (e.expired = true) : (e.expired = false);
+      });
+    }
+  },
+  methods: {
+    today() {
+      const rawToday = new Date();
+      const rawMonth = () => {
+        const month = rawToday.getMonth() + 1;
+        return month < 10 ? `0${month}` : month;
+      };
+      return `${rawToday.getFullYear()}-${rawMonth()}-${rawToday.getDate()}`;
+    },
+    computedDue(due) {
+      return new Date(`${due} GMT-0300`).toLocaleString("en-US", { month: "short", day: "2-digit" });
+    },
+    handleFnCall(fnName, taskId) {
+      return this[fnName](taskId);
+    },
+    snackBar(message) {
+      this.snackbar.text = message;
+      return (this.snackbar.active = true);
+    },
+    addTask() {
+      if (this.newTaskTitle === "") {
+        return this.snackBar("Please type a task");
+      }
+      else {
+        const idDate = Date.parse(new Date()) + (Math.floor(Math.random() * 10000000000000) + 1);
+        const newTask = {
+          id: idDate,
+          title: this.newTaskTitle,
+          dueDate: null,
+          expired: false,
+          done: false,
+          modal: false,
+      };
+      return this.tasks.push(newTask), (this.newTaskTitle = ""), this.snackBar("Task added!");
+      }
+    },
+    saveTask(refs, index, obj, message) {
+      return refs.dialog[index].save(obj), this.snackBar(message);
+    },
+    doneTask(taskID) {
+      const task = this.tasks.filter((task) => task.id === taskID)[0];
+      task.done = !task.done;
+      return task.done && task.done === true ? this.snackBar("Task is done!") : this.snackBar("Task is incomplete!");
+    },
+    deleteTask(taskID) {
+      this.tasks = this.tasks.filter((task) => task.id !== taskID);
+      return this.snackBar("Task removed!");
+    },
+  },
+  watch: {
+    tasks: {
+      handler(addTask) {
+        localStorage.tasks = JSON.stringify(addTask);
+      },
+      deep: true,
+    },
+  },
+};
+</script>
